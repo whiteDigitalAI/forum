6479bcfb21a7e8447803a04278c38538c347e183
GunaSeelan <GunaSeelan@192.168.7.2>
2019-11-26 17:27:07 +0530

    First Commit

---
+++ tests/Feature/ThreadsTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class ThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+    
+    public function test_a_user_can_browse_threads()
+    {
+        $response = $this->get('/threads');
+
+        $response->assertStatus(200);
+    }
+}
