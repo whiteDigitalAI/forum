98563ce7c7a42caedec157085dc0837ace60ca7e
guna <gunanadar@gmail.com>
2019-11-28 06:24:28 +0530

    episode 4

---
+++ tests/Feature/ReadThreadsTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+
+class ReadThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+    
+//    protected function setUp(): void
+//    {
+//        
+//        parent::setUp();
+//        
+//        $this->thread = factory('App\Thread')->create();
+//        
+//    }
+    
+    public function test_a_user_can_view_all_threads()
+    {
+        $thread = factory('App\Thread')->create();
+        
+        $response = $this->get('/threads')
+                    ->assertSee($thread->title);
+
+    }
+    
+     public function test_a_user_can_read_a_single_thread()
+    {
+       $thread = factory('App\Thread')->create();
+ 
+        $this->get('/threads/' . $thread->id)
+                ->assertSee($thread->title);
+
+    }
+    
+   
+}
