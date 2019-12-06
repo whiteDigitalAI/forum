3f3188f75438fa334b732f4a1c5e99472d3525d7
guna <gunanadar@gmail.com>
2019-12-06 06:35:33 +0530

    episode 20

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if($this->app->isLocal()){
+            
+            $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
+            
+        }
     }
 
     /**
@@ -25,14 +29,27 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \View::share('channels', \App\Channel::all());
-//        
-//        \View::composer( '*' . function($view){
-//             
-//             $view->with('channels', Channel::all());
-//             
-//             });
+       
+        
+        \View::composer( '*', function ($view){
              
+             $channels = \Cache::rememberForever('channels', function(){
+             
+             return \App\Channel::all();
+             
+             });
+
+             $view->with('channels', $channels);
+             
+             });
+             
+        
+//        \View::composer('*', function ($view) {
+//            $channels = \Cache::rememberForever('channels', function() {
+//                        return \App\Channel::all();
+//                    });
+//            $view->with('channels', $channels);
+//        });
     }         
         
 }
